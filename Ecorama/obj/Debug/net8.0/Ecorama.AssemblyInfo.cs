5ef6f612c97cc44214ad7fe720c8d30175c2cdee
KhalidMigdadi <khalidmigdadi1@gmail.com>
2025-06-13 19:39:54 +0300

    newUpdate

--- Ecorama/obj/Debug/net8.0/Ecorama.AssemblyInfo.cs
+++ Ecorama/obj/Debug/net8.0/Ecorama.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecorama")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceddd19e7c2cbaedaac4d593e5c920a43cd44ab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22175fa590a83ccdf8d0a88ecd26b133ff60c458")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecorama")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecorama")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
